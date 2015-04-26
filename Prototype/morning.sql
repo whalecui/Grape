6733ae6c5b01cb8a3bdb05c70f7f1ddb0fa50b40
morningmoni <morningmoni@163.com>
2015-04-26 21:14:27 +0800

    Database of group

---
+++ Prototype/morning.sql
@@ -0,0 +1,25 @@
+Drop Database if exists Grape;
+Create Database Grape;
+use Grape;
+
+Drop Table if exists groups;
+Create Table groups(
+name varchar(30) not null ,
+leadername varchar(30) not null
+);
+
+
+Drop Table if exists groupMemberAssosiation;
+Create Table groupMemberAssosiation(
+groupname varchar(30) not null ,
+membername varchar(30) not null
+);
+
+-- Drop Table if exists user;
+-- Create Table user(
+-- username varchar(30) not null ,
+-- password varchar(30) not null ,
+-- role int not null default 0,
+
+
+-- );
