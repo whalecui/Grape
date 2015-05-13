baf527ce8e08abe3a48932a8f6fe4474b49e6462
morningmoni <morningmoni@163.com>
2015-05-13 21:28:20 +0800

    基本完成group部分。几个功能都写在了首页，显示在/group。 把bootstrap&jquery移到了本地 其他自己看吧

--- Grape/function.py
+++ Grape/function.py
@@ -5,22 +5,29 @@ from config import *
 class Group:
 
     def __init__(self, name):
-        self.groupname = name
+        ##名字与数据库中相同
+        self.name=name
+        data=self.get_data()
+        self.group_id = data['group_id']
+        self.topic=data['topic']
+        self.confirmMessage=data['confirmMessage']
+        self.leadername=data['leadername']
 
     def get_members(self):
         conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
         cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        cursor.execute("select membername from groupMemberAssosiation where groupname="+"'"+self.groupname+"';")
+        cursor.execute("select membername from groupMemberAssosiation where groupname="+"'"+self.name+"';")
         members=cursor.fetchall()
         conn.close()
         return members
-    def get_leader(self):
+
+    def get_data(self):
         conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
         cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        cursor.execute("select leadername from groups where name="+"'"+self.groupname+"';")
-        leadername=cursor.fetchall()
+        cursor.execute("select * from groups where name="+"'"+self.name+"';")
+        data=cursor.fetchall()
         conn.close()
-        return leadername      
+        return data[0]  
 
 class User:
 
@@ -28,19 +35,22 @@ class User:
         self.username = name
         self.email = email
 
-    def create_group(self,groupname):
+    def create_group(self,groupname,topic,confirmMessage):
         conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
         cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
         #判断是否存在
         cursor.execute("select name from groups where name='"+groupname+"';")
         exist=cursor.fetchall()
         if(exist):
+            print 'failed to create group :',groupname
             return False
         cursor.execute("insert into groupMemberAssosiation(groupname,membername) values(%s,%s);",(groupname,self.username))
         conn.commit()
-        cursor.execute("insert into groups(name,leadername) values(%s,%s);",(groupname,self.username))
+        cursor.execute("insert into groups(name,topic,confirmMessage,leadername) values(%s,%s,%s,%s);",\
+            (groupname,topic,confirmMessage,self.username))
         conn.commit()
         conn.close()
+        print 'created group successfully:',groupname
         return True
 
     def delete_group(self,groupname):
@@ -55,8 +65,10 @@ class User:
             cursor.execute("delete from groupMemberAssosiation where groupname='"+groupname+"';")
             conn.commit()
             conn.close()
+            print 'deleted group successfully :',groupname
             return True
         conn.close()
+        print 'failed to delete group :',groupname
         return False
 
     def get_groups(self):
@@ -72,12 +84,42 @@ class User:
     def join_group(self,groupname):
         conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
         cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        cursor.execute("select name from groups where name='"+groupname+"' and leadername='"+self.username+"';")
+        cursor.execute("select name from groups where name='"+groupname+"';")
         exist=cursor.fetchall()
         if(exist):
             cursor.execute("insert into groupMemberAssosiation(groupname,membername) values(%s,%s);",(groupname,self.username))
             conn.commit()
+            conn.close()
+            print 'joined group successfully :',groupname
+            return True
         conn.close()
+        print 'failed to join group :',groupname
+        return False
+
+    def quit_group(self,groupname):
+        conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
+        cursor=conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        cursor.execute("select * from groupMemberAssosiation where membername='"+self.username+"' and groupname='"+groupname+"';")
+        exist=cursor.fetchall()    
+        if(exist):
+            cursor.execute("delete from groupMemberAssosiation where membername='"+self.username+"' and groupname='"+groupname+"';")   
+            conn.commit()
+            #whether he's leader
+            cursor.execute("select name from groups where name='"+groupname+"' and leadername='"+self.username+"';")            
+            isLeader=cursor.fetchall()  
+            if(isLeader):
+                print "the user trying to quit is LEADER!"
+                cursor.execute("delete from groups where name='"+groupname+"';")
+                conn.commit()
+
+            conn.close()
+            print 'quit group successfully :',groupname
+            return True
+        print 'failed to quit group :',groupname
+        conn.close()
+        return False
+
+        
 
     def search_group(self,groupname):
         conn=MySQLdb.connect(host=db_config["db_host"],port=db_config["db_port"],user=db_config["db_user"],passwd=db_config["db_passwd"],db=db_config["db_name"],charset="utf8")
